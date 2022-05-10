b52ac643a08f94ec522ffd6db1e4050622e7b542
HanJiyoung <jiyounghaan@gmail.com>
2022-05-10 18:56:09 +0900

    과제 풀이 영상을 참고하여 TodoHttpHandler 수정

--- app/src/main/java/com/codesoom/assignment/TodoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/TodoHttpHandler.java
@@ -15,145 +15,129 @@ import java.util.stream.Collectors;
 
 public class TodoHttpHandler implements HttpHandler {
 
-    private List<Task> tasks = new ArrayList<>(); // 새로운 task가 생성되면 리스트에 저장됨
+    private List<Task> tasks = new ArrayList<>();
     private ObjectMapper objectMapper = new ObjectMapper();
-    private static Long id = 1L;
+    private Long newId = 0L;
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String method = exchange.getRequestMethod(); // HTTP 요청 메서드(GET, POST, PUT, PATCH, DELETE)
-        URI uri = exchange.getRequestURI(); // ex. /
-        String path = uri.getPath(); // ex. /tasks
+        String method = exchange.getRequestMethod();
+        URI uri = exchange.getRequestURI();
+        String path = uri.getPath();
+
+        if(path.equals("/tasks")) {
+            handleCollection(exchange, method);
+            return;
+        }
+
+        if(path.startsWith("/tasks/")) {
+            Long id = Long.parseLong(path.substring("/tasks/".length()));
+            handleItem(exchange, method, id);
+            return;
+        }
+    }
+
+    private String getBody(HttpExchange exchange) {
         InputStream inputStream = exchange.getRequestBody();
 
-        String body = new BufferedReader(new InputStreamReader(inputStream))
+        return new BufferedReader(new InputStreamReader(inputStream))
                 .lines()
-                .collect(Collectors.joining("\n")); // 여러 줄을 개행해서 받아옴
-
-        String content = "";
-        if(path.startsWith("/tasks")) { // path가 /tasks로 시작하는 요청들에 대한 처리
-            content = handlePathTasks(method, path, body);
-            send(exchange, 200, content);
-        }
+                .collect(Collectors.joining("\n"));
     }
 
     private void send(HttpExchange exchange, int statusCode, String content) throws IOException {
         exchange.sendResponseHeaders(200, content.getBytes().length);
 
-        // OutputStream 객체 생성 후 write()을 해줘야 클라이언트 측에 응답 내용이 전달됨
         OutputStream outputStream = exchange.getResponseBody();
         outputStream.write(content.getBytes(StandardCharsets.UTF_8));
         outputStream.flush();
         outputStream.close();
     }
 
-    // path가 /tasks인 요청들을 처리
-    private String handlePathTasks(String method, String path, String body) throws IOException {
-        int taskId;
-
-        switch (method) {
-            case "GET":
-                if(path.length() > 6) {
-                    taskId = getTaskId(path);
-                    return getOneTask(taskId);
-                }
-                else {
-                    return getAllTask();
-                }
-
-            case "POST":
-                if(!body.isBlank()) { // request body가 비어있지 않을 때만 처리
-                    return postTask(body);
-                }
-                break;
-
-            case "PUT":
-            case "PATCH":
-                taskId = getTaskId(path);
-                String newTitle = toTask(body).getTitle();
-                return updateTask(taskId, newTitle);
-
-            case "DELETE":
-                taskId = getTaskId(path);
-                deleteTask(taskId);
-                break;
+    private void handleCollection(HttpExchange exchange, String method) throws IOException {
+        if(method.equals("GET")) {
+            handleList(exchange);
         }
-        return null;
+
+        if(method.equals("POST")) {
+            handleCreate(exchange);
+        }
     }
 
-    // request body의 내용을 Task 객체로 만들기
+    private void handleItem(HttpExchange exchange, String method, Long id) throws IOException {
+        Task task = findTask(id);
+
+        if (task == null) {
+            send(exchange, 404, "");
+            return;
+        }
+
+        if (method.equals("GET")) {
+            handleDetail(exchange, task);
+        }
+
+        if(method.equals("PUT") || method.equals("PATCH")) {
+            handleUpdate(exchange, task);
+        }
+
+        if(method.equals("DELETE")) {
+            handleDelete(exchange, task);
+        }
+    }
+
+    private void handleList(HttpExchange exchange) throws IOException {
+        send(exchange, 200, toJSON(tasks));
+    }
+
+    private void handleCreate(HttpExchange exchange) throws IOException {
+        String body = getBody(exchange);
+
+        Task task = toTask(body);
+        task.setId(generateId());
+        tasks.add(task);
+
+        send(exchange, 201, toJSON(task));
+    }
+
+    private void handleDetail(HttpExchange exchange, Task task) throws IOException {
+        send(exchange, 200, toJSON(task));
+    }
+
+    private void handleUpdate(HttpExchange exchange, Task task) throws IOException {
+        String body = getBody(exchange);
+        Task source = toTask(body);
+
+        task.setTitle(source.getTitle());
+
+        send(exchange, 200, toJSON(task));
+    }
+
+    private void handleDelete(HttpExchange exchange, Task task) throws IOException {
+        tasks.remove(task);
+        send(exchange, 200, "");
+    }
+
+    private Long generateId() {
+        newId += 1;
+        return newId;
+    }
+
     private Task toTask(String content) throws JsonProcessingException {
-        Task task = objectMapper.readValue(content, Task.class);
-        task.setId(id);
-        id = id + 1; // 할 일을 새로 생성할 때마다 id가 증가하도록 함
-
-        System.out.println("id: " + task.getId() + ", title: " + task.getTitle());
-
-        return task;
+        return objectMapper.readValue(content, Task.class);
     }
 
-    // tasks 리스트를 json 형태로 반환
-    private String tasksToJSON() throws IOException {
+    private String toJSON(Object object) throws IOException {
         OutputStream outputStream = new ByteArrayOutputStream();
-        objectMapper.writeValue(outputStream, tasks);
+        objectMapper.writeValue(outputStream, object);
 
         return outputStream.toString();
     }
 
-    // path에 포함되어있는 task id 추출
-    private int getTaskId(String path) {
-        return Integer.parseInt(path.substring(path.lastIndexOf('/') + 1));
+    private Task findTask(Long id) {
+        return tasks.stream()
+                .filter(task -> task.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 
-    // 할 일 목록 조회
-    private String getAllTask() throws IOException {
-        return tasksToJSON();
-    }
-
-    // 상세 조회
-    private String getOneTask(int taskId) {
-        String idAndTitle = null;
-
-        for(Task task : tasks) {
-            if(task.getId() == taskId) {
-                idAndTitle = task.toString();
-                break;
-            }
-        }
-
-        return idAndTitle;
-    }
-
-    // 할 일 생성하기
-    private String postTask(String body) throws JsonProcessingException {
-        Task task = toTask(body); // body의 내용을 Task 객체로 만듦
-        tasks.add(task); // 리스트에 task 추가
-
-        return task.toString(); // 내가 생성한 task 정보를 출력
-    }
-
-    // 할 일 제목 수정하기
-    private String updateTask(int taskId, String newTitle) {
-        String idAndTitle = null;
-
-        for(Task task : tasks) {
-            if(task.getId() == taskId) {
-                task.setTitle(newTitle);
-                idAndTitle = task.toString();
-                break;
-            }
-        }
-
-        return idAndTitle;
-    }
-
-    // 할 일 삭제하기
-    private void deleteTask(int taskId) {
-        for(Task task : tasks) {
-            if(task.getId() == taskId) {
-                tasks.remove(task);
-                break;
-            }
-        }
-    }
 }
